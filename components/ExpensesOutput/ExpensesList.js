467beb0bd6f0404ed15c7faf7b9681b51f73f77b
Utasi Bence <mac@Utasi-MacBook-Air.local>
2022-11-06 20:58:39 +0100

    finished project

---
+++ components/ExpensesOutput/ExpensesList.js
@@ -0,0 +1,21 @@
+import { StyleSheet, FlatList } from 'react-native';
+
+import ExpenseItem from './ExpenseItem';
+
+function renderExpenseItem(itemData) {
+  return <ExpenseItem {...itemData.item} />;
+}
+
+function ExpensesList({ expenses }) {
+  return (
+    <FlatList
+      data={expenses}
+      renderItem={renderExpenseItem}
+      keyExtractor={(item) => item.id}
+    />
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default ExpensesList;
